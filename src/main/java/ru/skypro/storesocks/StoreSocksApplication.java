943733d7e9438ba2de852f9128f969676189a46d
Kazackoffyury <kazachkoff.yury@yandex.ru>
2023-01-29 19:09:03 +0300

    initial

---
+++ src/main/java/ru/skypro/storesocks/StoreSocksApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.storesocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreSocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/storesocks/StoreSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.storesocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
